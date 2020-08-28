1a0808fee4a890bd73bd0142c2c342721b276bf4
vkUkad <vikentiy.kelevich@ukad-group.com>
2020-08-28 13:12:38 +0300

    Updated urls and selectors

--- src/test/java/Helpers/SetIgnoredElements.java
+++ src/test/java/Helpers/SetIgnoredElements.java
@@ -7,7 +7,7 @@ import java.util.Set;
 
 public class SetIgnoredElements {
     public static Set<By> setTheElements() {
-        Set<By> set = new HashSet() {
+        Set<By> set = new HashSet<By>() {
         };
         set.add(By.id("header"));
         return set;
